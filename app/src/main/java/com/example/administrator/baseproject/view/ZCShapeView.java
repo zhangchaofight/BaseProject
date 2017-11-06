a3349843e61841daf95bf543e233067fd93b276b
zhangchao <zhangchao@syswin.com>
2017-11-06 11:01:53 +0800

    完成自定义六边形View

--- app/src/main/java/com/example/administrator/baseproject/view/ZCShapeView.java
+++ app/src/main/java/com/example/administrator/baseproject/view/ZCShapeView.java
@@ -1,6 +1,7 @@
 package com.example.administrator.baseproject.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,40 +10,90 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
+import com.example.administrator.baseproject.R;
+
 /**
  * Created by Administrator on 2017/11/2.
  */
 
 public class ZCShapeView extends View {
 
-    private float width = 60f;
-    private float k = 1.73f;
+    private static final int DEFAULT_WIDTH = 200;
+    private static final float K = 1.73f;
+
+    private float mWidth;
+    private int mColor;
 
     public ZCShapeView(Context context) {
         super(context);
+        mColor = context.getResources().getColor(R.color.orange);
+        mWidth = context.getResources().getDimension(R.dimen.ZCShapeViewSize) / 4;
+        getAttrs(context, null);
     }
 
     public ZCShapeView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mColor = context.getResources().getColor(R.color.orange);
+        mWidth = context.getResources().getDimension(R.dimen.ZCShapeViewSize) / 4;
+        getAttrs(context, attrs);
     }
 
     public ZCShapeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mColor = context.getResources().getColor(R.color.orange);
+        mWidth = context.getResources().getDimension(R.dimen.ZCShapeViewSize) / 4;
+        getAttrs(context, attrs);
+    }
+
+    private void getAttrs(Context context, AttributeSet attributeSet) {
+        TypedArray typedArray = context.
+                obtainStyledAttributes(attributeSet, R.styleable.ZCShapeView);
+        mWidth = typedArray.getDimensionPixelSize(R.styleable.ZCShapeView_size, DEFAULT_WIDTH) / 4;
+        mColor = typedArray.getColor(R.styleable.ZCShapeView_color, mColor);
+        typedArray.recycle();
+    }
+
+    private int getWidthHeight(int measureSpec) {
+        int mode = MeasureSpec.getMode(measureSpec);
+        int size = MeasureSpec.getSize(measureSpec);
+
+        int mSize = DEFAULT_WIDTH;
+        switch (mode) {
+            case MeasureSpec.UNSPECIFIED:
+                mSize = DEFAULT_WIDTH;
+                break;
+            case MeasureSpec.AT_MOST:
+            case MeasureSpec.EXACTLY:
+                mSize = size;
+                break;
+        }
+        return mSize / 4;
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int width = getWidthHeight(widthMeasureSpec);
+        int height = getWidthHeight(heightMeasureSpec);
+
+        int size = width <= height ? width : height;
+        mWidth = size / 4;
+        setMeasuredDimension(size, size);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Paint paint = new Paint();
-        paint.setColor(Color.GRAY);
+        paint.setColor(mColor);
         Path path = new Path();
-        path.moveTo(width, 0);
-        path.lineTo(3 * width, 0);
-        path.lineTo(4 * width, width * k);
-        path.lineTo(3 * width, 2 * width * k);
-        path.lineTo(width, 2 * width * k);
-        path.lineTo(0, width * k);
-        path.lineTo(width, 0);
+        path.moveTo(mWidth, (float) 0.27 * mWidth);
+        path.lineTo(3 * mWidth, (float) 0.27 * mWidth);
+        path.lineTo(4 * mWidth, mWidth * K + (float) 0.27 * mWidth);
+        path.lineTo(3 * mWidth, 2 * mWidth * K + (float) 0.27 * mWidth);
+        path.lineTo(mWidth, 2 * mWidth * K + (float) 0.27 * mWidth);
+        path.lineTo(0, mWidth * K + (float) 0.27 * mWidth);
+        path.lineTo(mWidth, 0 + (float) 0.27 * mWidth);
         path.close();
         canvas.drawPath(path, paint);
     }
