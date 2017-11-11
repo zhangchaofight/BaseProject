00543d29af31e4acbdd818d5b5318dc9ffb14ad4
zhangchao <zhangchao@syswin.com>
2017-11-11 15:51:09 +0800

    finish pulldown intercept

--- app/src/main/java/com/example/administrator/baseproject/ThreeTypePullRefresh/OnTouchVG.java
+++ app/src/main/java/com/example/administrator/baseproject/ThreeTypePullRefresh/OnTouchVG.java
@@ -7,12 +7,17 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Scroller;
+import android.widget.Toast;
+
+import com.example.administrator.baseproject.view.ZCToast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +28,14 @@ import java.util.List;
 
 public class OnTouchVG extends LinearLayout{
 
-    private final String TAG = OnTouchVG.class.getSimpleName();
+    private Context mContext = getContext();
+
+    private ValueAnimator animator;
+
+    private LayoutParams params;
+
+    private int slop;
+    private int lastY;
 
     public OnTouchVG(Context context) {
         this(context, null);
@@ -39,83 +51,107 @@ public class OnTouchVG extends LinearLayout{
     }
 
     private void init() {
-        Log.d(TAG, "init: EXECUTED");
+        slop = ViewConfiguration.get(mContext).getScaledTouchSlop();
         setOrientation(LinearLayout.VERTICAL);
         addListView();
     }
 
-    @Override
-    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
-        super.onScrollChanged(l, t, oldl, oldt);
-        Log.d(TAG, "onScrollChanged: " + getScrollY());
-    }
-
-    private Context mContext = getContext();
-    private Scroller mScroller = new Scroller(getContext());
     private void addListView() {
         ListView lv = new ListView(mContext);
-        LayoutParams params0 = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+        params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT);
-        lv.setLayoutParams(params0);
+        lv.setLayoutParams(params);
         List<String> list = new ArrayList();
         for (int i = 0; i < 30; i++) {
             list.add("no: " + i);
         }
         lv.setAdapter(new ArrayAdapter<String>
                 (mContext, android.R.layout.simple_expandable_list_item_1, list));
-        lv.setPressed(false);
-        lv.setFocusable(false);
-        lv.setFocusableInTouchMode(false);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(mContext, "aaa", Toast.LENGTH_SHORT).show();
+            }
+        });
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(mContext, "bbb", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
         addView(lv);
     }
 
     @Override
-    public boolean dispatchTouchEvent(MotionEvent ev) {
-        Log.d(TAG, "dispatchTouchEvent: EXECUTED");
-        return super.dispatchTouchEvent(ev);
-    }
-
-    @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        return true;
-    }
-
-    int last = 0;
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        int y = (int) event.getRawY();;
-        Log.d(TAG, "onTouchEvent: EXECUTED");
-        switch (event.getAction()) {
+        int nowY = (int) ev.getRawY();
+        switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                last = (int) event.getRawY();
-                Log.d(TAG, "onTouchEvent: DOWN EXECUTED");
+                lastY = nowY;
                 break;
             case MotionEvent.ACTION_MOVE:
-                scrollBy(0, last - y);
-                Log.d(TAG, "onTouchEvent: MOVE EXECUTED distance : " + getScrollY());
-                Log.d(TAG, "onTouchEvent: MOVE EXECUTED");
+                int move = nowY - lastY;
+                if (Math.abs(move) <= slop) {
+                    break;
+                }
+                if (isListViewTopShowing() && move > slop) {
+                    params.topMargin = 0;
+                    return true;
+                }
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                Log.d(TAG, "onTouchEvent: UP EXECUTED");
-                playAnimation(getScrollY());
                 break;
         }
-        last = y;
+        lastY = nowY;
+        return super.onInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int y = (int) event.getRawY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int move;
+                move = lastY - y;
+                params = (LayoutParams) getChildAt(0).getLayoutParams();
+                params.topMargin -= move;
+                getChildAt(0).setLayoutParams(params);
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                playAnimation();
+                break;
+        }
+        lastY = y;
         return true;
     }
 
-    private void playAnimation(int start) {
-        ValueAnimator animator = ValueAnimator.ofInt(start, 0);
-        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+    private void playAnimation() {
+        animator = ValueAnimator.ofInt(params.topMargin, 0);
         animator.setDuration(200);
         animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
                 int i = (int) (valueAnimator.getAnimatedValue());
-                OnTouchVG.this.scrollTo(0, i);
+                params.topMargin = i;
+                getChildAt(0).setLayoutParams(params);
             }
         });
         animator.start();
+    }
+
+    private boolean isListViewTopShowing() {
+        ListView view = (ListView) getChildAt(0);
+        int pos = view.getFirstVisiblePosition();
+        int topMagin = -1;
+        if (null != view.getChildAt(pos)) {
+            topMagin = view.getChildAt(pos).getTop();
+        }
+        return (pos == 0) && (topMagin == 0);
     }
 }
